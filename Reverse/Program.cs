584fd02f7f3ef1c2f063828e1fc941d4aa2a8929
Richard Maness <rickrmaness@gmail.com>
2021-02-03 18:06:13 -0500

    Add project files.

---
+++ Reverse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+
+            //Initial String
+            Console.WriteLine();
+            Console.Clear();
+            string characters = Console.ReadLine();
+            length = characters.Length;
+
+            //Reverse String Instructions 
+            
+                for (int i = length - 1; i >= 0; i--)
+                {
+                    char singleLetter = characters[i];
+                    Console.Write(singleLetter);
+                
+                }
+                    // Palindrome test for string
+
+
+
+
+                
+            
+        }
+
+    }
+
+}
