da1a368244c1ed7e8754fdd12377e299785e4563
Richard Maness <rickrmaness@gmail.com>
2021-02-15 19:18:55 -0500

    added functionality to check for palindrome

--- Reverse/Program.cs
+++ Reverse/Program.cs
@@ -9,28 +9,33 @@ namespace Reverse
             int length;
 
             //Initial String
-            Console.WriteLine();
-            Console.Clear();
+            Console.WriteLine("Please Enter a String");
             string characters = Console.ReadLine();
             length = characters.Length;
 
             //Reverse String Instructions 
-            
-                for (int i = length - 1; i >= 0; i--)
-                {
-                    char singleLetter = characters[i];
-                    Console.Write(singleLetter);
-                
-                }
-                    // Palindrome test for string 
-
-
-
-                b
-                
-            
+            for (int i = length - 1; i >= 0; i--)
+            {
+                char singleLetter = characters[i];
+                Console.Write(singleLetter);
+            }
+            // Palindrome test for string 
+            string reverseCharacters = " ";
+            for (int i = reverseCharacters.Length - 1; i > 0; i--)
+            {
+                reverseCharacters += characters[i];
+            }
+            if (characters.Equals(reverseCharacters, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine();
+                Console.Write(" Thats a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.Write("Thats not a palindrome");
+            }
         }
-
     }
 
 }
